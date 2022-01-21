3f8b3872fe20a1895147b40a7964917f1b153c7b
Christoforos Vradis <chrisvradis00@gmail.com>
2022-01-21 17:48:00 +0200

    feat(css): add css

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, id }) => {
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
       <img alt="robots" src={`https://robohash.org/${id}?size=200x200`}></img>
       <div>
-        <h2>{name}</h2>
+        <h2 className="f5">{name}</h2>
         <p>{email}</p>
       </div>
     </div>
