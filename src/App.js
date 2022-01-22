1bfa9cb68b4349cc27e686573216b07b2c4f7580
Christoforos Vradis <chrisvradis00@gmail.com>
2022-01-22 17:21:13 +0200

    feat(): add scroll component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import axios from 'axios';
 import './App.css';
 
@@ -29,11 +30,16 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    }
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />;
+        <Scroll>
+          <CardList robots={filteredRobots} />;
+        </Scroll>
       </div>
     );
   }
---
+++ src/Scroll.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowY: 'scroll',
+        border: '5px solid black',
+        height: '800px',
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+export default Scroll;
